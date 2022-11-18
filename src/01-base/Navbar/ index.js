ccf7e25aa800473030b8b995441d3482e3e9c134
wendy <463710868@qq.com>
2022-11-18 20:54:34 +0800

    fix：属性的验证之静态属性static

--- src/01-base/Navbar/ index.js
+++ src/01-base/Navbar/ index.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import wendyProtypes from 'prop-types';
+console.log('属性验证的方法', wendyProtypes);
 export default class Navbar extends Component {
+    // 静态的方法，可直接使用，无需实例化
+    static Navbar = {
+        title: wendyProtypes.string,
+        leftShow: wendyProtypes.bool,
+        rightShow: wendyProtypes.bool
+    }
   render() {
-    const {title, leftShow, rightShow} = this.props;
+    const { title, leftShow, rightShow } = this.props;
+    console.log(title, leftShow, rightShow);
     return (
       <div>
         {leftShow && <button>back</button>}
@@ -12,3 +20,9 @@ export default class Navbar extends Component {
     )
   }
 }
+// 类的属性
+// Navbar.prototypes = {
+//     title: wendyProtypes.string,
+//     leftShow: wendyProtypes.bool,
+//     rightShow: wendyProtypes.bool
+// }
