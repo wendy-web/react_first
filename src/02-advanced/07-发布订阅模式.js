ca7f378819bab0650b0acf3d28a13225e7147dd2
wendy <463710868@qq.com>
2022-11-19 15:15:54 +0800

    feat：纯js的发布订阅的模式

---
+++ src/02-advanced/07-发布订阅模式.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>发布订阅模式</p>
+      </div>
+    )
+  }
+}
+let bus = {
+    list: [],
+    // 订阅者的模式
+    subscribe (callback){
+        callback();
+        this.list.push(callback)
+    },
+    // 发布者的模式
+    publish() {
+        // 遍历所有的订阅者，进行发布
+        this.list.forEach(callback => {
+            callback && callback();
+        })
+
+    }
+}
+bus.subscribe(() => {
+    console.log('000')
+})
+bus.subscribe(() => {
+    console.log('111')
+})
