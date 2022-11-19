84b6579da8a75fb6160df30e185efb55dfea0a49
wendy <463710868@qq.com>
2022-11-19 10:59:39 +0800

    feat：子传父数据的传递

---
+++ src/02-advanced/01-子传父.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+class Navbar extends Component {
+    render() {
+      return (
+        <div>
+            Navbar
+            <button onClick={() => {
+                // console.log("执行navbar的事件");
+                // console.log('父组件中传递过来的函数', this.props.event);
+                this.props.event(); // 直接的调用执行
+        }}>Navbar - sidebar 的开关</button>
+        </div>
+      )
+    }
+}
+
+class Sidebar extends Component {
+    render() {
+        return (
+        <div>
+            Sider的组件展示内容
+        </div>
+        )
+    }
+}
+  
+export default class App extends Component {
+    state= {
+        isShow: false
+    }
+
+  render() {
+    return (
+      <div>
+        <Navbar event= {() => {
+            console.log("父组件定义的event的事件,传递回调函数去使用");
+            this.setState({
+                isShow: !this.state.isShow
+            })
+        }}></Navbar>
+        { this.state.isShow && <Sidebar></Sidebar> }
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ import ReactDom from 'react-dom';
 // import App from './01-base/19-非受控组件';
 // import App from './01-base/20-受控组件';
 // import App from './01-base/21-受控cinema';
-import App from './01-base/22-受控todolist';
+// import App from './01-base/22-受控todolist';
 
-
+import App from './02-advanced/01-子传父';
 
 
 
