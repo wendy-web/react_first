027b8b56c321d438dd5308cc8844a052127596ce
wendy <463710868@qq.com>
2022-11-20 21:38:05 +0800

    feat: 生命周期更新的SCU的使用

---
+++ src/02-advanced/15-更新阶段生命周期2.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state={
+    nameValue: 'wendy'
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("获取最新的状态：", this.state.nameValue, nextState.nameValue);
+    if(this.state.nameValue !== nextState.nameValue) {
+      return true;
+    }
+    return false;
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('更新阶段，更新完成；componentDidUpdate');
+  }
+  
+  render() {
+    console.log('render')
+    return (
+      <div>
+        <button onClick={() => {
+          this.setState({
+            nameValue: 'stateName'
+          })
+        }}>click更改state的值</button>
+        <p>{this.state.nameValue}</p>
+      </div>
+    )
+  }
+}
