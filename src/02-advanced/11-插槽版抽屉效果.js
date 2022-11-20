eafacee9838b82bad6028c95c221002383884f26
wendy <463710868@qq.com>
2022-11-20 19:53:37 +0800

    feat：插槽-组件的复用

---
+++ src/02-advanced/11-插槽版抽屉效果.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+class Navbar extends Component {
+    render() {
+      return (
+        <div>
+            Navbar
+            {/* 留好组件的插槽位置 */}
+            {this.props.children}
+        </div>
+      )
+    }
+}
+
+class Sidebar extends Component {
+    render() {
+        return (
+        <div>
+            Sider的组件展示内容
+        </div>
+        )
+    }
+}
+  
+export default class App extends Component {
+    state= {
+        isShow: false
+    }
+
+  render() {
+    return (
+      <div>
+        <Navbar>
+          {/* 插槽的内容 */}
+          <button onClick={() => {
+            this.setState({
+              isShow: !this.state.isShow
+            })
+          }}>Navbar - sidebar 的开关</button>
+        </Navbar>
+        <hr />
+        <p>底下是展示sider的组件： isShow的值控制：</p>
+        { this.state.isShow && <Sidebar></Sidebar> }
+      </div>
+    )
+  }
+}
