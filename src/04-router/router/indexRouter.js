1e5f2be72790fa99270e2e07f2e8b32669c85558
wendy <463710868@qq.com>
2022-11-22 13:13:24 +0800

    feat：router路由的重定向

--- src/04-router/router/indexRouter.js
+++ src/04-router/router/indexRouter.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Films from '../views/films';
 import Cinemas from '../views/cinemas';
 import Center from '../views/center';
+import NotFound from '../views/NotFound';
 export default class indexRouter extends Component {
   render() {
     return (
         <HashRouter>
-            <Route path='/films' component={Films}></Route>
-            <Route path='/cinemas' component={Cinemas}></Route>
-            <Route path='/center' component={Center}></Route>
+            {/* 相当于switch的匹配，匹配上就跳出 */}
+            <Switch>
+                <Route path='/films' component={Films}></Route>
+                <Route path='/cinemas' component={Cinemas}></Route>
+                <Route path='/center' component={Center}></Route>
+                
+                {/* 重定向的默认的路由 - 模糊匹配 */}
+                {/* exact关键字是精确匹配 */}
+                <Redirect from='/' to='center' exact></Redirect>
+
+                {/* 404 not found */}
+                <Route component={NotFound}></Route>
+            </Switch>
         </HashRouter>
     )
   }
