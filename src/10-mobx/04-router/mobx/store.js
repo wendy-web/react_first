05d871de55309c804c6658a517dceef8e09c33c1
wendy <463710868@qq.com>
2022-11-23 20:11:33 +0800

    feat：mobx的异步

--- src/10-mobx/04-router/mobx/store.js
+++ src/10-mobx/04-router/mobx/store.js
@@ -1,4 +1,5 @@
-import {observable, configure, action} from 'mobx';
+import axios from 'axios';
+import {observable, configure, action, runInAction} from 'mobx';
 
 // 过于自由,使用严格模式，使用action的方法更改，而不是在外部任意的更改
 configure({
@@ -21,7 +22,6 @@ configure({
 // });
 // export default store;
 
-
 // 面向对象的
 class store {
     @observable show = true;
@@ -33,6 +33,23 @@ class store {
     }
     @action changeHide() {
         this.show = false;
+    }
+    @action getList() {
+        axios({
+            url: 'https://m.maizuo.com/gateway?cityId=440300&pageNum=1&pageSize=10&type=1&k=3293746',
+            method: 'get',
+            headers: {
+              'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.1","e":"16687675555277788957310977","bc":"440300"}',
+              'X-Host': 'mall.film-ticket.cinema.list'
+            }
+          }).then((res) => {
+            // 异步的修改 runInAction
+            runInAction(() => {
+                console.log(res.data.data.cinemas)
+                this.list = res.data.data.cinemas;
+            })
+
+        });
     }
 }
 export default new store();
--- src/10-mobx/04-router/views/cinemas.js
+++ src/10-mobx/04-router/views/cinemas.js
@@ -1,17 +1,40 @@
-/*
- * @Author: wendy 463710868@qq.com
- * @Date: 2022-11-22 12:55:22
- * @LastEditors: wendy 463710868@qq.com
- * @LastEditTime: 2022-11-22 12:55:37
- * @FilePath: /react_first_wendy/src/04-router/views/cinemas.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import React, { Component } from 'react'
-
-export default class cinemas extends Component {
-  render() {
-    return (
-      <div>cinemas</div>
-    )
-  }
+import { autorun } from 'mobx';
+import React, { useEffect, useState } from 'react';
+import store from '../mobx/store';
+function Cinemas(props) {
+  const [list, setlist] = useState([]);
+  console.log(list)
+  useEffect(() => {
+    if(store.list.length === 0) {
+      store.getList();
+    }
+    // 监听list的更改
+    const unsubscribe = autorun(() => {
+      console.log(store.list);
+      setlist(store.list)
+    });
+    return() => {
+      // 销毁时取消订阅
+      unsubscribe();
+    }
+  }, [])
+  return (
+    <div>
+      <div>
+        <p onClick={() => {
+          props.history.push('city');
+        }}>点击进入cinemas的城市：{store.cityName}</p>
+        <button onClick={() => {
+          props.history.push('/cinemas/search')
+        }}>搜索</button>
+      </div>
+      <hr />
+      <ul>
+        {list.map((item) => {
+          return <li key={item.cinemaId}>{item.cinemaId}-{item.name}</li>
+        })}
+      </ul>
+    </div>
+  )
 }
+export default Cinemas;
