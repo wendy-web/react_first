991b604445939b5930eb83b361fedb32d946506d
wendy <463710868@qq.com>
2022-11-23 19:24:22 +0800

    feat：mobx使用严格模式的调用

--- src/10-mobx/04-router/mobx/store.js
+++ src/10-mobx/04-router/mobx/store.js
@@ -1,10 +1,23 @@
-import {observable, autorun} from 'mobx';
+import {observable, configure, action} from 'mobx';
 
-
+// 过于自由,使用严格模式，使用action的方法更改，而不是在外部任意的更改
+configure({
+    enforceActions: 'always'
+})
 const store = observable({
     list: [],
     cityName: 'biejing',
-    show: true
+    show: true,
+    changeShow(){
+        this.show = true;
+    },
+    changeHide() {
+        this.show = false;
+    }
+}, {
+    // 标记俩个方法是action，专门修改可观测的value
+    changeHide:action,
+    changeShow:action
 })
 
 export default store;
