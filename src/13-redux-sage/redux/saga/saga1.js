468c2466c0a6c28ef8a70300d3505e92acfcab1c
wendy <463710868@qq.com>
2022-11-26 00:55:38 +0800

    feat: saga的写法更改

--- src/13-redux-sage/redux/saga/saga1.js
+++ src/13-redux-sage/redux/saga/saga1.js
@@ -1,11 +1,15 @@
-import {take, fork, call, put} from 'redux-saga/effects';
+import {take, fork, takeEvery, call, put} from 'redux-saga/effects';
 function *watchSaga1() {
-    while(true) {
-        // take监听组件发来的action
-        yield take('get_list1');
-        yield fork(getList1);
-        // fork 同步的执行异步处理函数
-    }
+    // 写法一：
+    // while(true) {
+    //     // take监听组件发来的action
+    //     yield take('get_list1');
+    //     yield fork(getList1);
+    //     // fork 同步的执行异步处理函数
+    // }
+
+    // 写法二：
+    yield takeEvery('get_list1', getList1)
 }
 function *getList1() {
     // call函数发异步请求
--- src/13-redux-sage/redux/saga/saga2.js
+++ src/13-redux-sage/redux/saga/saga2.js
@@ -1,11 +1,14 @@
-import {take, fork, call, put} from 'redux-saga/effects';
+import {take, fork, takeEvery, call, put} from 'redux-saga/effects';
 function *watchSaga2() {
-    while(true) {
-        // take监听组件发来的action
-        yield take('get_list2');
-        yield fork(getList2);
-        // fork 同步的执行异步处理函数
-    }
+    // while(true) {
+    //     // take监听组件发来的action
+    //     yield take('get_list2');
+    //     yield fork(getList2);
+    //     // fork 同步的执行异步处理函数
+    // }
+
+    yield takeEvery('get_list2', getList2)
+
 }
 function *getList2() {
     // call函数发异步请求
